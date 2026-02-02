d6d407decf2f6b915219c8b60bd6f25696b52e3e
Farheen Fahim <farheenfahim100@Gmail.com>
2026-02-02 21:43:53 +0000

    fixed run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,4 @@
 exec gunicorn project:app \
   --worker-class gevent \
   --workers 1 \
-<<<<<<< HEAD
   --bind 0.0.0.0:$PORT
-=======
-  --bind 0.0.0.0:$PORT
->>>>>>> 7600fff25f82c5fa95e4544d75b78ed57b388aa0
