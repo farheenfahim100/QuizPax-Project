5e8979d0efa078cfa4f40df4a434acc9e50b75fb
Farheen Fahim <farheenfahim100@Gmail.com>
2026-02-06 19:43:00 +0000

    suppress send email is false by default

--- project.py
+++ project.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 
 
 
-app.config['MAIL_SUPPRESS_SEND'] = os.environ.get("MAIL_SUPPRESS_SEND", "true").lower() == "true"
+app.config['MAIL_SUPPRESS_SEND'] = os.environ.get("MAIL_SUPPRESS_SEND", "false").lower() == "true"
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
