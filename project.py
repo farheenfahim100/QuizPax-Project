168de3cad3897d68235a1c72a56609a61c4ab78b
Farheen Fahim <farheenfahim100@Gmail.com>
2026-02-06 19:11:54 +0000

    bug for sending emails fixed x2

--- project.py
+++ project.py
@@ -275,7 +275,7 @@ def generate_otp(email,emailMsg):
 
 def send_email(recipient, body, subject_to_send):
   with app.app_context():
-    msg = Message(subject_to_send, sender=app.MAIL_USERNAME, recipients=[recipient])
+    msg = Message(subject_to_send, sender=app.config['MAIL_USERNAME'], recipients=[recipient])
     msg.body = body
     if ("@" not in recipient):
       return False
